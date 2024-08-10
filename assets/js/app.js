d4210795b53bfbbb0a8887cbcb864dd8cbcf33fa
Mahmud Ghali <mahmudghali01@gmail.com>
2024-08-10 03:32:15 +0100

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -38,27 +38,11 @@ scrollUp.addEventListener("click", () => {
   });
 });
 
-const navSlide = () => {
-  const burger = document.querySelector('.burger');
-  const nav = document.querySelector('.nav-links');
-  const navLinks = document.querySelectorAll('.nav-links li');
+// document.addEventListener('DOMContentLoaded', function() {
+//   const burger = document.querySelector('.burger');
+//   const nav = document.querySelector('.nav-links');
 
-  burger.addEventListener('click', () => {
-    // Toggle Nav
-    nav.classList.toggle('nav-active');
-
-    // Animate Links
-    navLinks.forEach((link, index) => {
-      if (link.style.animation) {
-        link.style.animation = '';
-      } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-      }
-    });
-
-    // Burger Animation
-    burger.classList.toggle('toggle');
-  });
-}
-
-navSlide();
+//   burger.addEventListener('click', function() {
+//     nav.classList.toggle('show');
+//   });
+// });
